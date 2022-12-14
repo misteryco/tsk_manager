3769be11b9ab6bc62d534f1b1e132414b758e996
misteryco <jordan.v.dimov@gmail.com>
2022-12-14 13:24:40 +0200

    removed a function view for task create

--- TaskManager/tasks/urls.py
+++ TaskManager/tasks/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 
 from TaskManager.tasks.views import TasksCreateView, TaskDetailsView, TaskEditView, task_list_funct_view, \
-    task_delete, task_create_view
+    task_delete
 
 urlpatterns = [
     path('', task_list_funct_view, name='tasks list'),
     path('create/', TasksCreateView.as_view(), name='task create'),
-    path('create2/', task_create_view, name='task create2'),
     path('<int:pk>/', include([
         path('details/', TaskDetailsView.as_view(), name='task detail'),
         path('edit/', TaskEditView.as_view(), name='task edit'),
